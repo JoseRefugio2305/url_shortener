95394c5d82615073ab034247532c78183a1412fe
JoseRefugio2305 <joserefugioriveramendoza@gmail.com>
2025-09-24 21:48:27 -0600

    +Ruta activa en navbar

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -8,9 +8,11 @@ import {
 } from "flowbite-react";
 import viteLogo from "/icono.png";
 import { useAuth } from "../hooks/useAuth";
+import { useLocation } from "react-router-dom";
 
 function NavBarCmp() {
    const { token, logout } = useAuth();
+   const route = useLocation();
 
    return (
       <Navbar fluid className="shadow-md">
@@ -21,24 +23,24 @@ function NavBarCmp() {
                alt="Shorten URL Logo"
             />
             <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
-               Shortening URL
+               Shortening URL 
             </span>
          </NavbarBrand>
          <NavbarToggle />
          <NavbarCollapse>
-            <NavbarLink href="/" className="text-sm sm:text-xl">
+            <NavbarLink href="/" className="text-sm sm:text-xl" active={route.pathname==="/"}>
                Home
             </NavbarLink>
 
             {!token ? (
                <>
-                  <NavbarLink href="/login" className="text-sm sm:text-xl">
+                  <NavbarLink href="/login" className="text-sm sm:text-xl"active={route.pathname==="/login"} >
                      Iniciar Sesión/Registrarse
                   </NavbarLink>
                </>
             ) : (
                <>
-                  <NavbarLink href="/dashboard" className="text-sm sm:text-xl">
+                  <NavbarLink href="/dashboard" className="text-sm sm:text-xl" active={route.pathname==="/dashboard"}>
                      Dasboard
                   </NavbarLink>
                   <Button color="alternative" pill onClick={logout}>Cerrar sesión</Button>
